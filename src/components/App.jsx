63a67edd6bce156edd042e2b764323b5b5346904
Natalia <davydenko.n@ukr.net>
2022-11-20 19:00:39 +0200

    added project structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,13 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-    </div>
-  );
+import { Component } from "react";
+// import { AppStyled } from "./App.styled";
+
+export class App extends Component {
+  state = {
+
+  }
+
+//   return(
+// // <AppStyled>
+// // </AppStyled >
+// //   );
 };
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,1 @@
+import { SearchbarStyled } from "./SearchbarStyled";
